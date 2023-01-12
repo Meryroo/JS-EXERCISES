7ab71da117c5a31edd6130330a79bfea489db00b
meryroo <meryrooprada@gmail.com>
2023-01-12 15:47:54 +0100

    first commit

---
+++ JS-LOOPS/iteracion1.js
@@ -0,0 +1,19 @@
+/*Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
+ Usa la función .includes de javascript.*/
+
+const list = ['Camiseta de Pokemon', 
+'Pantalón coquinero', 
+'Gorra de gansta', 
+'Camiseta de Basket', 
+'Cinrurón de Orión', 
+'AC/DC Camiseta']
+
+
+for (const item of list) {
+    console.log(item.includes("Camiseta"));
+    
+}
+    
+
+
+    
